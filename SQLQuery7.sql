120532bb2b52fc2c11d3d74f29e5c67665f6e865
Blue9232 <112566205+Blue9232@users.noreply.github.com>
2022-09-08 19:12:39 +0300

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[Nume]
-      ,[Prenume]
-      ,[NumarTelefon]
-      ,[Oras]
-  FROM [Clienti].[dbo].[Detalii Clienti]
